3a7525a5d619c4ec7861238c41a4bdaa6bf50dce
z1rtaee <piresbeatrizwrk@gmail.com>
2024-11-19 17:46:36 +0000

    ESTA A IR

--- ft_printf.c
+++ ft_printf.c
@@ -6,16 +6,88 @@
 /*   By: bpires-r <bpires-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 17:11:28 by bpires-r          #+#    #+#             */
-/*   Updated: 2024/11/18 19:08:02 by bpires-r         ###   ########.fr       */
+/*   Updated: 2024/11/19 17:40:13 by bpires-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_printf(const char *format, ...)
+int	ft_printf(const char *s, ...)
 {
+	int	res;
 	va_list arg_lst;
 	
-	va_start(arg_lst, format);
-	
+	res = 0;
+	// if (!s)
+	// 	return (0);
+	va_start(arg_lst, s);
+	while (s)
+	{
+		if (*s == '%')
+		{
+			s++;
+			
+		}
+		else
+			write(1, &*s, 1);
+		s++;
+	}
+}
+
+int	str_format(const char *s, va_list arg_lst)
+{
+	int	res;
+	int	c;
+
+	res = 0;
+	c = 0;
+	if (*s == 'c')
+	{
+		c = va_arg(arg_lst, int)
+		res = res + write(1, &c, 1)
+	}
+	else if (*s == 's')
+		res = res + ft_putstr(va_arg(arg_lst, char *));
+	else if (*s == 'd' || *s == 'i')
+		res = res + ft_putnbr()
+}
+
+int	ft_putstr(char *s)
+{
+	int res;
+
+	res = 0;
+	while (*s)
+	{
+		res = res + write(1, &*s, 1);
+		s++;
+	}
+	return (res);
+}
+
+int	ft_putnbr(char *s, long n, int base)
+{
+	int	res;
+	int	c;
+
+	res = 0;
+	if (n < 0 && base == 10)
+	{
+		write (1, "-", 1);
+		n = -n;
+		if (n > 0)
+			ft_putnbr(n / 10);
+		c = '0' + (n % 10);
+		res = res + write(1, &c, 1);
+	}
+	if (n < 0 && base == 16)
+	{
+		write (1, "-", 1);
+		n = -n;
+		if (n > 0)
+			ft_putnbr(n / 16);
+		c = '0' + (n % 16);
+		res = res + write(1, &c, 1);
+	}
+	return (res);
 }
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: bpires-r <bpires-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 19:42:02 by zirtaee           #+#    #+#             */
-/*   Updated: 2024/11/18 18:40:36 by bpires-r         ###   ########.fr       */
+/*   Updated: 2024/11/19 16:16:55 by bpires-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,17 @@
 # include <stdio.h>
 # include <stdarg.h>
 
+# ifndef DECIMAL
+#  define DECIMAL "0123456789"
+# endif
+# ifndef HEXA_UP
+#  define HEXA_UP "0123456789ABCDEF"
+# endif
+# ifndef HEXA_LOW
+# define HEXA_LOW "0123456789abcdef"
+# endif
 
 size_t	ft_strlen(const char *s);
+int		ft_printf(const char *s, ...);
 
 #endif
